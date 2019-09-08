3debbaa71afd57aeacb39baaa40f2f4ddb28c8cf
cq823251117 <823251117@qq.com>
2019-09-08 15:14:17 +0800

    add check cookies

--- src/main/java/com/example/community/mapper/UserMapper.java
+++ src/main/java/com/example/community/mapper/UserMapper.java
@@ -3,6 +3,8 @@ package com.example.community.mapper;
 import com.example.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +13,8 @@ import org.springframework.stereotype.Repository;
 public interface UserMapper {
     @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
+
+    @Select("select * from user where token = #{token}")
+    //当方法的参数不是类的时候，就需要用@Param，是类的时候就不需要，就如上面的insert方法，因为传入的是User类参数，所以没有使用@Param注解
+    User finByToken(@Param("token") String token);
 }
