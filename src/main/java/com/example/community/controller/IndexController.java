384776f2c93ce590627c1fddfc11f012c9dcc7ad
cq823251117 <823251117@qq.com>
2019-09-09 22:53:36 +0800

    首页问题列表功能

--- src/main/java/com/example/community/controller/IndexController.java
+++ src/main/java/com/example/community/controller/IndexController.java
@@ -1,13 +1,17 @@
 package com.example.community.controller;
 
+import com.example.community.dto.QuestionDTO;
 import com.example.community.mapper.UserMapper;
+import com.example.community.service.QuestionService;
 import com.example.community.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
 public class IndexController {
@@ -15,20 +19,28 @@ public class IndexController {
     @Autowired
     private UserMapper userMapper;
 
+    @Autowired
+    private QuestionService questionService;
+
     @GetMapping("/")
-    public String index(HttpServletRequest request){
+    public String index(HttpServletRequest request,
+                        Model model){
         Cookie[] cookies=request.getCookies();
-        for (Cookie cookie : cookies){
-            if(cookie.getName().equals("token")){
-                String token = cookie.getValue();
-                User user = userMapper.finByToken(token);
-                if(user != null){
-                    request.getSession().setAttribute("user",user);
+        if (cookies!= null&&cookies.length!=0) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("token")) {
+                    String token = cookie.getValue();
+                    User user = userMapper.finByToken(token);
+                    if (user != null) {
+                        request.getSession().setAttribute("user", user);
+                    }
+                    break;
                 }
-                break;
             }
         }
 
+        List<QuestionDTO> questionsList=questionService.list();
+        model.addAttribute("questions",questionsList);
         return "index";
     }
 }
--- src/main/java/com/example/community/mapper/UserMapper.java
+++ src/main/java/com/example/community/mapper/UserMapper.java
@@ -11,10 +11,13 @@ import org.springframework.stereotype.Repository;
 
 @Mapper
 public interface UserMapper {
-    @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
+    @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified,avatar_url) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified},#{avatarUrl})")
     void insert(User user);
 
     @Select("select * from user where token = #{token}")
     //当方法的参数不是类的时候，就需要用@Param，是类的时候就不需要，就如上面的insert方法，因为传入的是User类参数，所以没有使用@Param注解
     User finByToken(@Param("token") String token);
+
+    @Select("select * from user where id=#{id}")
+    User finById(@Param("id") Integer id);
 }
