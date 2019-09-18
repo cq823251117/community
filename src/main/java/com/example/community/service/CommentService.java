db8568190aa501e255b7a60fa69459e049b58115
cq823251117 <823251117@qq.com>
2019-09-18 22:09:32 +0800

    实现回复功能-添加事物（bug依旧）

--- src/main/java/com/example/community/service/CommentService.java
+++ src/main/java/com/example/community/service/CommentService.java
@@ -10,6 +10,7 @@ import com.example.community.model.Comment;
 import com.example.community.model.Question;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CommentService {
@@ -22,6 +23,8 @@ public class CommentService {
 
     @Autowired
     private QuestionExtMapper questionExtMapper;
+
+    @Transactional
     public void insert(Comment comment) {
         if (comment.getParentId()==null||comment.getParentId()==0){
             throw new CustomizeException(CustomizeErrorCode.TARGET_PARAM_NOT_FOUND);
