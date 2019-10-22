aa72bff219bd0405f316c2ac610ecba18f5bbba4
cq823251117 <823251117@qq.com>
2019-10-22 23:05:33 +0800

    修复通知功能（二级评论存在bug）

--- src/main/java/com/example/community/interceptor/SessionInterceptor.java
+++ src/main/java/com/example/community/interceptor/SessionInterceptor.java
@@ -3,6 +3,7 @@ package com.example.community.interceptor;
 import com.example.community.mapper.UserMapper;
 import com.example.community.model.User;
 import com.example.community.model.UserExample;
+import com.example.community.service.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -19,11 +20,14 @@ public class SessionInterceptor implements HandlerInterceptor {
     @Autowired
     private UserMapper userMapper;
 
+    @Autowired
+    private NotificationService notificationService;
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        Cookie[] cookies=request.getCookies();
+        Cookie[] cookies = request.getCookies();
         //通过cookie判断用户是否登录，存在用户数据则直接登录
-        if (cookies!= null&&cookies.length!=0) {
+        if (cookies != null && cookies.length != 0) {
             for (Cookie cookie : cookies) {
                 if (cookie.getName().equals("token")) {
                     String token = cookie.getValue();
@@ -32,6 +36,8 @@ public class SessionInterceptor implements HandlerInterceptor {
                     List<User> users = userMapper.selectByExample(userExample);
                     if (users.size() != 0) {
                         request.getSession().setAttribute("user", users.get(0));
+                        Long unreadCount = notificationService.unreadCount(users.get(0).getId());
+                        request.getSession().setAttribute("unreadCount",unreadCount);
                     }
                     break;
                 }
